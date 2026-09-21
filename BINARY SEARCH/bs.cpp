6f37737c69ea42757529b98bbd12ce0ccc535b09
venkataguruprasadG <venkatagp27@gmail.com>
2026-09-21 21:20:13 +0530

    binary search.

---
+++ BINARY SEARCH/bs.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(const vector<int> &arr, int target)
+{
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int target = 3;
+
+    int result = binarySearch(arr, target);
+}
